d765d012412841fec0d33dc88e1bc6048944bb26
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2023-06-11 18:03:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Dkmovie/Elsa.git /𒆜S P A R R O W𒆜
+  git clone https://github.com/Dkmovie/SPARROW.git /𒆜S P A R R O W𒆜
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /𒆜S P A R R O W𒆜
 fi
-cd /Shortner
+cd /𒆜S P A R R O W𒆜
 pip3 install -U -r requirements.txt
 echo "𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐬𝐩𝐚𝐫𝐫𝐨𝐰.....🧞‍♂️"
 python3 bot.py
